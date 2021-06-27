1135922a00e46fb20e78f0958b37e63c8b801e28
isaac <isaac@rubenfamily.com>
2021-06-26 23:09:16 -0700

    make a buildLib

--- build.js
+++ build.js
@@ -1,103 +1,5 @@
 const esbuild = require('esbuild')
-const Path = require('path')
-const { copyFile, unlink, readdir, mkdir } = require('fs/promises')
-const { watch, mkdirSync } = require('fs')
-
-const mLog = e => {
-  if (e.code !== 'EEXIST' && e.code !== 'EISDIR') console.log(e)
-}
-
-try { mkdirSync('build') }
-catch (e) { mLog(e) }
-
-const copyDir = (from, to, append) => {
-  if (append) {
-    from = `${from}/${append}`
-    to = `${to}/${append}`
-  }
-  readdir(from).then(async fArr => {
-    await mkdir(to, { recursive: true }).catch(mLog)
-    await Promise.all(fArr.map(file => copyFile(
-      `${from}/${file}`,
-      `${to}/${file}`
-    ).catch(mLog)))
-    console.log(`${from} copy done`)
-  })
-}
-
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-const syncDir = (from, to, append) => {
-  if (append) {
-    from = `${from}/${append}`
-    to = `${to}/${append}`
-  }
-  const fsDebounce = new Set()
-  watch(from, async (e, f) => {
-    if (fsDebounce.has(f)) return
-    fsDebounce.add(f)
-    await sleep(100)
-    fsDebounce.delete(f)
-    await copyFile(
-      `${from}/${f}`,
-      `${to}/${f}`,
-    ).catch(async err => {
-      if (err.code === 'ENOENT') {
-        await unlink(`${to}/${f}`)
-        console.log(`${to}/${f} was deleted`)
-      } else {
-        console.log(err)
-      }
-    })
-    console.log(`${f} was synced (${e})`)
-  })
-}
-
-const mirrorDir = async (from, to, append) => {
-  copyDir(from, to, append)
-  syncDir(from, to, append)
-}
-
-const copyFileArray = (from, to, arr) => Promise.all(
-  arr.map(path => copyFile(
-    `${from}/${path}`,
-    `${to}/${Path.basename(path)}`
-  ))
-).then(() => console.log(`${from}/[${arr.join(', ')}] copy done`))
-
-const syncFileArray = (from, to, arr) => {
-  arr.forEach(file => {
-    let debounce = false
-    watch(`${from}/${file}`, async (e, f) => {
-      if (debounce) return
-      debounce = true
-      await sleep(100)
-      debounce = false
-      await copyFile(
-        `${from}/${f}`,
-        `${to}/${f}`,
-      ).catch(async err => {
-        if (err.code === 'ENOENT') {
-          await unlink(`${to}/${f}`)
-          console.log(`${to}/${f} was deleted`)
-        } else {
-          console.log(err)
-        }
-      })
-      console.log(`${f} was synced (${e})`)
-    })
-  })
-}
-
-const mirrorFileArray = (from, to, arr) => {
-  copyFileArray(from, to, arr)
-  syncFileArray(from, to, arr)
-}
-
-mirrorFileArray('src', 'build', [
-  'options.html',
-  'popup.html',
-])
+const bLib = require('./buildLib')
 
 esbuild.build({
   allowOverwrite: true,
@@ -114,5 +16,12 @@ esbuild.build({
   outdir: 'build'
 }).catch(() => process.exit(1))
 
-mirrorDir('browser', 'build')
-mirrorDir('browser', 'build', 'icons')
+bLib.ensureDir('build')
+
+bLib.Dir.mirror('browser', 'build')
+bLib.Dir.mirror('browser', 'build', 'icons')
+
+bLib.FileArray.mirror('src', 'build', [
+  'options.html',
+  'popup.html',
+])
