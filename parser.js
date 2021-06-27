4d9c442a8d89fa0b606d0180d64752567c2661af
isaac <isaac@rubenfamily.com>
2021-06-27 13:12:37 -0700

    functional basic line parsing

--- parser.js
+++ parser.js
@@ -4,10 +4,26 @@ const [input] = process.argv.slice(2)
 
 console.log(`attempting to parse ${input}`)
 
+
+const reg = {
+  domain: /(?<=^@).*/gm,
+  chunk: /(?<=^@)(.|\n)*?(?=\n@)/gm,
+}
+
 readFile(input, 'utf8')
   .then(str => {
-    console.log(str.match(/(?<=^@).*/gm))
+    str = `${str}
+@end
+`// this adds the eof to the file for easy parse
 
+
+    const chunks = str.match(reg.chunk)
+
+    chunks.forEach((chunk, i) => {
+      chunk = chunk.trim()
+      const lines = chunk.split('\n')
+      console.log(lines, i)
+    })
 
   })
   .catch(e => console.log(e))
