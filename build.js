aff67998533875d5aadedea515e2c1c56de6e11f
isaac <isaac@rubenfamily.com>
2021-06-26 19:44:31 -0700

    working build with watch

--- build.js
+++ build.js
@@ -1,12 +1,17 @@
 const esbuild = require('esbuild')
+let buildCount = 0
 
 esbuild.build({
   allowOverwrite: true,
+  watch: {
+    onRebuild: () => console.log(`watch build ${buildCount++} done!`),
+  },
+  bundle: true,
   entryPoints: [
-    'background',
-    'foreground',
-    'options',
-    'popup',
-  ].map(str => `src/${str}.js`),
+    'background.js',
+    'foreground.js',
+    'options.jsx',
+    'popup.jsx'
+  ].map(str => `src/${str}`),
   outdir: 'build'
 }).catch(() => process.exit(1))
