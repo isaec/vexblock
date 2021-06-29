d441d0aa54a1f297857c3dedf28302b1c807e7ab
isaac <isaac@rubenfamily.com>
2021-06-28 22:43:35 -0700

    basic sort

---
+++ sort.js
@@ -0,0 +1,21 @@
+const { readFile, writeFile } = require('fs/promises')
+const Path = require('path')
+
+const [input] = process.argv.slice(2)
+console.log(`attempting to sort ${input}`)
+
+const getName = str => str.trim().split('\n')[0]
+
+
+readFile(input, 'utf8')
+  .then(str => {
+    str = `${str}
+@end
+`// this adds the eof to the file for easy parse
+
+    const sorted = Array.from(str.matchAll(/(?<=^@)(.|\n)*?(?=\n@)/gm), m => m[0])
+      .sort((a, b) => getName(a).localeCompare(getName(b)))
+      .join('\n')
+
+    console.log(sorted)
+  })
