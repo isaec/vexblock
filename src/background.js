6226d48330a060794e6958decc424106d084b187
isaac <isaac@rubenfamily.com>
2021-06-28 17:46:07 -0700

    inject foreground script on new tab

--- src/background.js
+++ src/background.js
@@ -1,3 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('woot')
 })
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, { url, ...tab }) => {
+  if (changeInfo.status === 'complete' && /^http/.test(url)) {
+    chrome.scripting.executeScript({
+      target: { tabId },
+      files: ['./foreground.js'],
+    })
+      .then(() => console.log('injected.'))
+      .catch(e => console.log(e))
+  }
+})
