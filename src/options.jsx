be606ede69939f20e48467983e530675e31be3ab
isaac <isaac@rubenfamily.com>
2021-07-02 21:01:47 -0700

    working options

--- src/options.jsx
+++ src/options.jsx
@@ -1,1 +1,9 @@
-console.log('live!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './options/options.scss'
+
+const App = () => <div
+  className='App'
+>lmao</div>
+
+ReactDOM.render(<App />, document.querySelector('#root'))
