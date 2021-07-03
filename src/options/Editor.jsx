530b7e43f244fd25611123cd36f99dfddee5c500
isaac <isaac@rubenfamily.com>
2021-07-03 11:41:03 -0700

    safe syntax highlighting

--- src/options/Editor.jsx
+++ src/options/Editor.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import './Editor.scss'
+
+const sanitize = str => {
+  const temp = document.createElement('div')
+  temp.textContent = str
+  return temp.innerHTML
+}
 
 const Editor = () => {
 
   const [content, setContent] = useState('test!')
+  const highlighted = useMemo(() => ({
+    __html: sanitize(content)
+      .replace(
+        /^(@)(?<domain>[^\s]*)/gm,
+        '@<span style="color:red">$<domain></span>')
+  }), [content])
 
   return <div>
     <textarea
       id='edit'
       onChange={e => setContent(e.target.value)}
-    >{content}</textarea>
+      value={content}
+    ></textarea>
     <pre id='highlight' aria-hidden='true'>
-      <code>{content}</code>
+      <code
+        dangerouslySetInnerHTML={highlighted}
+      ></code>
     </pre>
   </div>
 }
